b348e9937cbe6c9be7704559fe87552b0252f7d2
SohamDasBiswas <sohamdb123@gmail.com>
2023-04-14 11:52:32 +0530

    Welcome Template Update

--- robotum.py
+++ robotum.py
@@ -1,17 +1,16 @@
 #============================================= All imports
 import datetime
-import discord,requests, sys, webbrowser, bs4
-from PIL import Image, ImageTk, ImageDraw, ImageFont, ImageFilter
-from discord.flags import Intents
+import discord,requests
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+# from discord.flags import Intents
 import os
-import discord
 import random
-import requests
 from io import BytesIO
 from dotenv import load_dotenv
 from discord.ext import commands
-from discord.ext import *
-from discord.ext.commands import Bot
+# from discord.ext import *
+# from discord.ext.commands import Bot
+from keep_alive import keep_alive
 
 #============================================= Intents
 intents = discord.Intents.all()
@@ -30,21 +29,20 @@ async def on_ready():
 @robotum.command()                                       
 async def hello(ctx):
     await ctx.send(f"Hello @{ctx.author}")
-    member = ctx.author
-    await ctx.send(member.avatar.url)
 
 #============================================= Member join welcome embed
 @robotum.event
 async def on_member_join(member):
     channel = robotum.get_channel(869448970416582746)
     embed=discord.Embed(title="𝓦𝓮𝓵𝓬𝓸𝓶𝓮!",description=f"{member.mention} Just Joined The Server.")
-    img = Image.open("Images/download.png")
+    backgrounds =os.listdir('Images')
+    random_bg = random.choice(backgrounds)
+    img = Image.open("Images/" + random_bg)
+    # print(random_bg)
     if member.avatar == None:
         font = ImageFont.truetype("ttf/KaushanScript-Regular.ttf", 75)
         font2 = ImageFont.truetype("ttf/KaushanScript-Regular.ttf", 80)
-
         text="Welcome"
-
         draw=ImageDraw.Draw(img)
         draw.text((600,100), text, (255, 255, 255), font=font, align='center')
         draw.text((300,300), f"{member}", (255, 255, 255), font=font2, align='center' )
@@ -59,7 +57,7 @@ async def on_member_join(member):
         mask_im = Image.new("L", (img2.width,img2.height), 0)
         draw2 = ImageDraw.Draw(mask_im)
         center_x, center_y = (175, 175)
-        radius = 122.5
+        radius = 150
         left = center_x - radius
         top = center_y - radius
         right = center_x + radius
@@ -68,26 +66,19 @@ async def on_member_join(member):
         draw2.ellipse(circle_box, fill=255)
         blur = mask_im.filter(ImageFilter.GaussianBlur(10))
         img.paste(img2, (575, 0), blur)
-
         font = ImageFont.truetype("ttf/KaushanScript-Regular.ttf", 75)
         font2 = ImageFont.truetype("ttf/KaushanScript-Regular.ttf", 80)
-        # Image.Image.paste(img,img2,(575,10), ())
-
         text="Welcome"
-
         draw=ImageDraw.Draw(img)
         draw.text((600,300), text, (255, 255, 255), font=font, align='center')
         draw.text((300,400), f"{member}", (255, 255, 255), font=font2, align='center' )
-
         img.save("Images/text.png")
         file = discord.File("Images/text.png", filename="Welcome.png")
         embed.set_image(url="attachment://Welcome.png")
-        # embed.set_thumbnail(url=member.avatar.url)
         embed.timestamp = datetime.datetime.now()
         print (member)
     await channel.send(file=file, embed=embed)
     await member.send("Welcome to the Official KGEC Robotics Society Discord Server! \n\nThis is Robotum, the official bot of the server. We are glad that you joined us! 🤗 \n Please Check rules and information in the Server")
-    # await channel.send(member.avatar)
 
 #============================================= Member left embed
 @robotum.event
@@ -100,4 +91,5 @@ async def on_member_remove(member:discord.member):
 
 #============================================= Fetching token from .env
 load_dotenv()
+keep_alive()
 robotum.run(os.getenv("TOKEN"))
